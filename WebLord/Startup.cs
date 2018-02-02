84af5481f0068728a4e92417f6865b40cef873d7
Firas Alkhatib <masterkey.no5@hotmail.com>
2018-02-02 17:51:00 +0200

    init

---
+++ WebLord/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebLord.Startup))]
+namespace WebLord
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
